814ca48f22781e0093907555fa01c6a8c3258ef4
Joseph <josephwaizy@gmail.com>
2026-04-01 22:05:14 -0500

    more tuning next transitional tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -9,11 +10,19 @@ import com.pedropathing.ftc.localization.constants.ThreeWheelConstants;
 import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
 
 public class Constants {
 
+
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(10);
+            .mass(10.25)
+            .headingPIDFCoefficients(new PIDFCoefficients(2,0,0.00001,0.055))
+            .useSecondaryTranslationalPIDF(true)
+            .useSecondaryHeadingPIDF(true)
+            .useSecondaryDrivePIDF(true)
+            .forwardZeroPowerAcceleration(-26.2366605549332)
+            .lateralZeroPowerAcceleration(-60.780323877099995);
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
@@ -27,13 +36,14 @@ public class Constants {
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .xVelocity(73.10214152906667)
+            .yVelocity(53.5788395452)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
 
 
 
     public static ThreeWheelConstants localizerConstants = new ThreeWheelConstants()
             .forwardTicksToInches(0.002984549)
-            .strafeTicksToInches(-0.270314864)
+            .strafeTicksToInches(-0.00297651915)
             .turnTicksToInches(0.001445051897062565)
             .leftPodY(8.5)
             .rightPodY(-6)
@@ -43,7 +53,7 @@ public class Constants {
             .strafeEncoder_HardwareMapName("leftBack")
             .leftEncoderDirection(Encoder.REVERSE)
             .rightEncoderDirection(Encoder.FORWARD)
-            .strafeEncoderDirection(Encoder.FORWARD);
+            .strafeEncoderDirection(Encoder.REVERSE);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
